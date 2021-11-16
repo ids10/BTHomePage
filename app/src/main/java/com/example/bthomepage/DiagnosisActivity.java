144918e6053bac34cebb40950b4f40cb9c267ed7
ids10 <isha.dipen.shah@hotmail.com>
2021-11-16 12:06:23 -0500

    Commit1116.2

--- app/src/main/java/com/example/bthomepage/DiagnosisActivity.java
+++ app/src/main/java/com/example/bthomepage/DiagnosisActivity.java
@@ -3,15 +3,18 @@ package com.example.bthomepage;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class DiagnosisActivity extends AppCompatActivity{
+public class DiagnosisActivity extends AppCompatActivity {
 
 
-        @Override
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnosis);
@@ -19,7 +22,27 @@ public class DiagnosisActivity extends AppCompatActivity{
 //            bottomNavigationView = findViewById(R.id.bottomNav_view);
 //            bottomNavigationView.setOnItemSelectedListener(this);
 
-        }
+    }
+    public void sendMessageCOPD(View view) {
+        Intent intent = new Intent(this, COPDDescript.class);
+        startActivity(intent);
+    }
+
+    public void sendMessageCF(View view) {
+        Intent intent = new Intent(this, CFDescript.class);
+        startActivity(intent);
+    }
+    public void sendMessageCB(View view) {
+        Intent intent = new Intent(this, CBDescript.class);
+        startActivity(intent);
+    }
+    public void sendMessageAsthma(View view) {
+        Intent intent = new Intent(this, AsthmaDescript.class);
+        startActivity(intent);
+    }
+
+
+
 //        DiagnosisActivity diagnosisactivity = new DiagnosisActivity();
 //        LungsActivity lungsactivity = new LungsActivity();
 //        DietActivity dietactivity = new DietActivity();
@@ -46,4 +69,4 @@ public class DiagnosisActivity extends AppCompatActivity{
 //    }
 
 
-}
+    }
