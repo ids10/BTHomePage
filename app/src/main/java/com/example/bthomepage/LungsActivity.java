a52890c73eec9580460747964f681e281095a908
ids10 <isha.dipen.shah@hotmail.com>
2021-11-16 14:13:41 -0500

    Commit1116.4

--- app/src/main/java/com/example/bthomepage/LungsActivity.java
+++ app/src/main/java/com/example/bthomepage/LungsActivity.java
@@ -2,7 +2,9 @@ package com.example.bthomepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LungsActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class LungsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lungs);
+        
+
     }
+    public void sendMessagePLBreathing(View view) {
+        Intent intent = new Intent(this, PLBreathing.class);
+        startActivity(intent);
+    }
+    public void sendMessageACBT(View view) {
+        Intent intent = new Intent(this, ACBT.class);
+        startActivity(intent);
+    }
+    public void sendMessageAutoDrainage(View view) {
+        Intent intent = new Intent(this, AutoDrainage.class);
+        startActivity(intent);
+    }
+    public void sendMessageDiaphram(View view) {
+        Intent intent = new Intent(this, Diaphram.class);
+        startActivity(intent);
+    }
+    public void sendMessagePV(View view) {
+        Intent intent = new Intent(this, PV.class);
+        startActivity(intent);
+    }
+    public void sendMessagedrainpos(View view) {
+        Intent intent = new Intent(this, drainpos.class);
+        startActivity(intent);
+    }
+
 }
